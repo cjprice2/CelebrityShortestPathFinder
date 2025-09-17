35c0f0161e4ea1a3afbc429a56da15ad24e3fb36
cjprice2 <colinjamesprice04@gmail.com>
2025-09-17 17:42:52 -0500

    hopefully it works this time

--- backend/src/main/java/com/example/CelebrityShortestPathFinderApplication.java
+++ backend/src/main/java/com/example/CelebrityShortestPathFinderApplication.java
@@ -14,6 +14,8 @@ public class CelebrityShortestPathFinderApplication {
     public static void main(String[] args) {
         // Force server to bind to 0.0.0.0 instead of localhost
         System.setProperty("server.address", "0.0.0.0");
+        System.out.println("DEBUG: server.address system property set to: " + System.getProperty("server.address"));
+        System.out.println("DEBUG: SERVER_ADDRESS env var: " + System.getenv("SERVER_ADDRESS"));
         SpringApplication.run(CelebrityShortestPathFinderApplication.class, args);
     }
 
